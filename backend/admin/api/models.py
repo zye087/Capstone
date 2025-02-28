2f23e2109efe1edf77c00745df08bf89e81ee3e7
zye <zye.github.01@gmail.com>
2025-02-28 14:50:48 +0800

    updates

--- backend/admin/api/models.py
+++ backend/admin/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        db_table = 'student'
+
+class parentGuardian(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        db_table = 'parents_guardians'
