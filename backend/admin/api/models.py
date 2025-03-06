00314547ccfc7004d6e1d19831a5a9b7211532fa
zye <zye.github.01@gmail.com>
2025-03-06 15:10:06 +0800

    updates

--- backend/admin/api/models.py
+++ backend/admin/api/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 class student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    suffix = models.CharField(max_length=10, null=True, blank=True)
+    section = models.CharField(max_length=15, null=True, blank=True)
+    gender = models.CharField(max_length=10, null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    status = models.CharField(max_length=15, null=True, blank=True)
 
     def __str__(self):
         return self.first_name
--- frontend/src/components/student/List.jsx
+++ frontend/src/components/student/List.jsx
@@ -16,7 +16,7 @@ import {
 
 const List = () => {
   const [data, setData] = React.useState([])
-  
+
   React.useEffect(() => {
     fetch('http://127.0.0.1:8000/student/')
       .then(response => response.json())
@@ -25,21 +25,54 @@ const List = () => {
 
   return (
     <>
-      <TableContainer component={Paper} elevation={4} sx={{ maxHeight: '600px', mt: '10px' }}>
-        <Table stickyHeader> 
+      <TableContainer
+        component={Paper}
+        elevation={4}
+        sx={{
+          maxHeight: '600px',
+          mt: '10px'
+          }}
+      >
+        <Table
+          stickyHeader
+        > 
           <TableHead>
             <TableRow>
-              <TableCell>Student ID#</TableCell>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', backgroundColor: '#1976d2', color: 'white' }}>Student ID#</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', backgroundColor: '#1976d2', color: 'white' }}>First Name</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', backgroundColor: '#1976d2', color: 'white' }}>Last Name</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', backgroundColor: '#1976d2', color: 'white' }}>Section</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', backgroundColor: '#1976d2', color: 'white' }}>Gender</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', backgroundColor: '#1976d2', color: 'white' }}>Age</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', backgroundColor: '#1976d2', color: 'white' }}>Address</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', backgroundColor: '#1976d2', color: 'white' }}>Status</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {data.map(item => (
-              <TableRow key={item.id}>
+              <TableRow
+                key={item.id}
+                component='tr'
+                sx={{
+                  ':hover': {
+                    backgroundColor: '#42a5f5',
+                    cursor: 'pointer',
+                    color: 'white'
+                  }
+                }}
+              >
                 <TableCell>{item.id}</TableCell>
                 <TableCell>{item.first_name}</TableCell>
                 <TableCell>{item.last_name}</TableCell>
+                <TableCell>{item.section}</TableCell>
+                <TableCell>{item.gender}</TableCell>
+                <TableCell>{item.age}</TableCell>
+                <TableCell>{item.address}</TableCell>
+                <TableCell
+                  sx={{
+                    color: (item.status == 'Active' ? 'green' : 'red')
+                  }}
+                >{item.status}</TableCell>
               </TableRow>
             ))}
           </TableBody>
